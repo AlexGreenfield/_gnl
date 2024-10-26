6ddec27b7fd44ca387bd41039fc8f08d4c7288f3
Alejandro Castroverde Rodrigo <acastrov@c1r10s6.42madrid.com>
2024-10-26 17:15:56 +0200

    changes in gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,71 +6,69 @@
 /*   By: acastrov <acastrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:30:18 by acastrov          #+#    #+#             */
-/*   Updated: 2024/10/24 21:18:18 by acastrov         ###   ########.fr       */
+/*   Updated: 2024/10/26 17:15:34 by acastrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 // Leaks cause not join yet
-static char	*ft_read(int fd, char **saved);
+char	*ft_read(int fd, char **saved);
 char	*ft_free(char **to_free, int flag);
-char *ft_split_saved(char **saved);
+static char *ft_split_saved(char **saved);
 
 char	*get_next_line(int fd)
 {
 	static char	*saved;
-	char	*line_return;
+	char		*line_return;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0) // Check for invalid size of not fd
 		return (NULL);
-	if (!saved || ft_strchr_n(saved) != NULL)
+	if (!saved) // If there's nothing on saved, we read until next n or EOF
 		ft_read(fd, &saved);
-	line_return = NULL;
-	if (ft_strchr_n(saved) != NULL)
-	{
+	while (saved && ft_strchr_n(saved) == NULL)
+		saved = ft_strjoin(saved, ft_read(fd, &saved)); // May cause leaks?
+	if (ft_strchr_n(saved) != NULL) // If there's N in saved, we must split saved and create line_return
 		line_return = ft_split_saved(&saved);
-		return (line_return);
-	}
-	else
+	else // If there's no N, we return the rest of saved. Now it fails cause of this, we need to read more
 	{
 		line_return = ft_strdup(saved);
-		free (saved);
-		saved = NULL;
-		return (line_return);
-	}
-	if (!line_return) // This causes doble free, may need fix later
 		return (ft_free(&saved, 2));
-	//after read, substring separar linea retorno y el resto de guardar con __stub_sigreturn cada free de estatica con = NULL;
+	}
+	if (!line_return)
+		return (ft_free(&saved, 2));
+	return (line_return);
 }
 
-static char	*ft_read(int fd, char **saved)
+char	*ft_read(int fd, char **saved)
 {
 	ssize_t	bytes_readed;
 	char	*buf;
 
-	buf = malloc(BUFFER_SIZE + 1);
+	buf = malloc(BUFFER_SIZE + 1); // We alloc buffer and read
+	if (!buf)
+		return(NULL);
 	bytes_readed = read(fd, buf, BUFFER_SIZE);
-	if (bytes_readed < 0)
+	if (bytes_readed < 0) // Check for fail, return null
 		return (ft_free(&buf, 0));
-	else if (bytes_readed == 0)
+	else if (bytes_readed == 0) // If not bytes readed, free buf and return empty string
 		return (ft_free(&buf, 1));
-	buf[BUFFER_SIZE] = '\0';
-	*saved = ft_strdup(buf);
+	buf[bytes_readed] = '\0'; // Always Null terminate
+	*saved = ft_strdup(buf); // First fill of saved, check for fail
 	if (!*saved)
 		return (ft_free(saved, 0));
-	while (bytes_readed > 0 && ft_strchr_n(*saved) == NULL)
+	while (bytes_readed > 0 && ft_strchr_n(*saved) == NULL) // If there's saved left, we read
 	{
 		bytes_readed = read(fd, buf, BUFFER_SIZE);
 		if (bytes_readed < 0)
 			return (ft_free(&buf, 0));
-		*saved = ft_strjoin(*saved, buf);
+		*saved = ft_strjoin(*saved, buf); // Join for keeping fill saved
 		if (!saved)
 			return (ft_free(saved, 2));
 	}
-	free (buf);
+	free (buf); // After we used buf, we free it
 	return (*saved);
 }
-char *ft_split_saved(char **saved)
+static char *ft_split_saved(char **saved)
 {
 	size_t	ptr_diff;
 	char	*next_n;
@@ -78,35 +76,24 @@ char *ft_split_saved(char **saved)
 	char	*line_return;
 
 	next_n = ft_strchr_n(*saved); // We need a join if saved left
-	//if (next_n != NULL)
-	//{
-		ptr_diff = next_n - *saved; // We calculate the size to be copied
-		line_return = ft_substr(*saved, 0, ptr_diff + 1); // We copy to new pointer
-		if (!line_return)
-			return (ft_free(saved, 2));
-		temp = ft_strdup(next_n + 1);
-		free (*saved);
-		*saved = temp;
-		if (!*saved)
-			return (ft_free(saved, 2));
-	//}
-	/*else
-	{
-		line_return = ft_strdup(*saved);
-		if (!line_return)
-			return (ft_free(saved, 2));
-		free(*saved);
-		*saved = NULL;
-	}*/
+	ptr_diff = next_n - *saved; // We calculate the size to be copied
+	line_return = ft_substr(*saved, 0, ptr_diff + 1); // We make a new string from actual string to n
+	if (!line_return)
+		return (ft_free(saved, 2));
+	temp = ft_strdup(next_n + 1); // Se save the string after n
+	free (*saved);
+	*saved = temp;
+	if (!*saved)
+		return (ft_free(saved, 2));
 	return (line_return);
 }
-
+// Frees pointer direction and returns different values according to flag
 char	*ft_free(char **to_free, int flag)
 {
 	free(*to_free);
-	if (flag == 1)
+	if (flag == 1) // If empty saved, we return empty string, may cause leaks cause not free?
 		return (ft_strdup(""));
-	if (flag == 2)
+	if (flag == 2) // If freeing saved, we must assign NULL value
 		*to_free = NULL;
 	return (NULL);
 }
